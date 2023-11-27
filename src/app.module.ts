e1b5a74226c55b24639b9f94c0bc1c94c89a8d30
Daniel Arriaga <danielgerman007@yahoo.com.ar>
2023-11-27 20:47:42 -0300

    Cambio para usar variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,20 @@ import { LibroModule } from './libro/libro.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Libro } from './libro/entities/libro.entity';
 import { Autor } from './autor/entities/autor.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     AutorModule,
     LibroModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '192.168.88.143',
-      port: 5432,
-      username: 'biblio',
-      password: 'secret',
-      database: 'biblioteca',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Autor, Libro],
       synchronize: true,
     }),
